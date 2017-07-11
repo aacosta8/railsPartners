5f42010db61f4ea1f162e10122addd07754a5c6d
Alex <aacosta@eafit.edu.co>
2017-07-11 00:15:18 -0500

    Inicio de sesión con facebook termindado

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,36 @@
 class Users::OmniauthCallbacksController < ApplicationController
+
   def facebook
     @User = User.from_omniauth(request.env["omniauth.auth"])
     if @User.persisted?
       @User.remember_me = true
       sign_in_and_redirect @User, event: :authentication
     end
+
+    session["devise.auth"] = request.env["omniauth.auth"]
+
+    render :edit
   end
+
+  def custom_sign_up
+    @User = User.from_omniauth(session["devise.auth"])
+    if @User.update(user_params)
+      sign_in_and_redirect @User, event: :authentication
+    else
+      render :edit
+    end
+  end
+
+  def failure
+    redirect_to new_user_session_path, notice: "No pudimos loguearte "\
+                                               "Error: #{params[:error_description]}."\
+                                               " Motivo: #{params[:error_reason]}"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:email,:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
 
   root 'main#home'
+  post "/custom_sign_up", to: "users/omniauth_callbacks#custom_sign_up"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
